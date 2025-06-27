3a650fcbd58872959c92224b195c177bd949b1b6
Gaël Louage <gaellouage@hotmail.com>
2025-06-27 12:46:15 +0200

    char counter

---
+++ CountCharsInStringEx/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var charCounter = Count("Hello, World!");
+        foreach (var item in charCounter)
+        {
+            Console.WriteLine($"Key: {item.Key} - Count: {item.Value}");
+        }
+    }
+
+    public static Dictionary<char, int> Count(string str)
+    {
+        var charCounter = new Dictionary<char, int>();
+        foreach (var c in str) 
+        {
+            if (charCounter.ContainsKey(c))
+            {
+                charCounter[c]++;
+            }
+            else
+            {
+               charCounter.Add(c, 1);
+            }
+        }
+        return charCounter;
+    }
+    public static Dictionary<char, int> CountLinq(string str)
+    {
+        return str.GroupBy(x => x).ToDictionary(c => c.Key, c => c.Count());
+    }
+}
